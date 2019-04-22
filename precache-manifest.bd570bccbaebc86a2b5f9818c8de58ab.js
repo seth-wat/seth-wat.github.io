0ae7935b30efc545de815406b0ac181771c2b784
Seth Watkins <dontpanic@Seths-MacBook-Pro.local>
2019-04-22 13:24:44 -0500

    feat: init

---
+++ precache-manifest.bd570bccbaebc86a2b5f9818c8de58ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0332fc5f6beb88f5e4fd",
+    "url": "/static/css/main.b7dbf2b0.chunk.css"
+  },
+  {
+    "revision": "0332fc5f6beb88f5e4fd",
+    "url": "/static/js/main.613cc25d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "fcdcece3c1dbcef10dc3",
+    "url": "/static/css/2.e99b752d.chunk.css"
+  },
+  {
+    "revision": "fcdcece3c1dbcef10dc3",
+    "url": "/static/js/2.3354391b.chunk.js"
+  },
+  {
+    "revision": "cabbee2d6ed50f28980fcd15e2ba09e8",
+    "url": "/index.html"
+  }
+];
